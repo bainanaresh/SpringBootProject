3e52c943f72a17f2d5d22913206ea484756af0e5
bainanaresh <bainanaresh407@gmail.com>
2024-09-14 11:16:31 +0530

    removed code

--- src/main/java/com/baina/CodingPractices.java
+++ src/main/java/com/baina/CodingPractices.java
@@ -12,28 +12,7 @@ public class CodingPractices {
 
 	public static void main(String[] args) {
 		
-		int arr[]= {5,7,3,9,1},temp;
-		
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		
-		for(int i=0;i<arr.length;i++) {
-			
-			for(int j=0;j<arr.length;j++) {
-				
-				if(arr[i]>arr[j]) {
-					temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
-				}
-				
-			}
-		}
-		System.out.println();
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
+		//removed code
 		
 	}
 
